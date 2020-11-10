9252c9a3c69b791ec79cf5875cb451f0748b8f96
Diego Gomez <diegofabricioggmail.com>
2020-11-10 13:11:20 -0600

    Autor controller y servicio

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoticiasApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NoticiasApi.Models;
+
+namespace NoticiasApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController:ControllerBase
+    {
+        private readonly AutorService _autorService;
+        public AutorController(AutorService autorService)
+        {
+            _autorService = autorService;
+        }
+
+        [HttpGet]
+        [Route("obtener")]
+        public IActionResult Obtener()
+        {
+            var resultado = _autorService.Obtener();
+            return Ok(resultado);
+        }
+    }
+}
---
+++ Services/AutorService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoticiasApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasApi.Services
+{
+    public class AutorService
+    {
+        private readonly DBContext _autorDbContext;
+        public AutorService(DBContext autorDbContext)
+        {
+            _autorDbContext = autorDbContext;
+        }
+        public List<Autor> Obtener()
+        {
+            try
+            {
+                var autores = _autorDbContext.Autor.ToList();
+                return autores;
+            }
+            catch(Exception error)
+            {
+                return new List<Autor>();
+            }            
+        }
+    }
+}
